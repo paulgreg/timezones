cf28c6fc260486d96a36a9e4eeeb9f2c420407a8
Grégory Paul <me@gpaul.me>
2023-02-04 13:24:47 +0100

    react: use react-select instead of native select

--- src/AddTimeZone.js
+++ src/AddTimeZone.js
@@ -1,4 +1,5 @@
-import React, { useState, useCallback, useEffect } from 'react'
+import React, { useState, useCallback, useRef } from 'react'
+import Select from 'react-select'
 import { getTimeZones, filterTimeZones, groupTimeZones, formatOffset, getCity, getCountry } from './TimeZonesHelper'
 import './AddTimeZone.css'
 
@@ -10,34 +11,36 @@ const displayCounty = (label) => {
 const AddTimeZone = ({ excludedTimezones, addFn }) => {
   const tzs = filterTimeZones(getTimeZones(), excludedTimezones)
   const timezoneGroups = groupTimeZones(tzs)
-  const firstTimeZone = tzs[0].label
-  const [selectValue, setSelectValue] = useState(firstTimeZone)
+  const [selectValue, setSelectValue] = useState(null)
+  const selectInputRef = useRef()
 
-  useEffect(() => setSelectValue(firstTimeZone), [firstTimeZone])
+  const addTimezone = useCallback(() => {
+    addFn(selectValue)
+    selectInputRef.current.clearValue()
+  }, [setSelectValue, addFn, selectValue])
 
-  const addTimezone = useCallback(() => addFn(selectValue), [addFn, selectValue])
+  const handleChange = useCallback((item) => setSelectValue(item?.value), [setSelectValue])
 
-  const handleChange = useCallback(
-    (e) => {
-      setSelectValue(e.target.value)
-    },
-    [setSelectValue]
-  )
+  const groupedOptions = Object.keys(timezoneGroups).map((group) => ({
+    label: group,
+    options: timezoneGroups[group].map((tz) => ({
+      value: tz.label,
+      label: `${getCity(tz.label)} ${displayCounty(tz.label)} ${formatOffset(tz.offset)}`,
+    })),
+  }))
 
   return (
     <div className="add-timezone">
-      <select value={selectValue} onChange={handleChange}>
-        {Object.keys(timezoneGroups).map((group) => (
-          <optgroup key={group} label={group}>
-            {timezoneGroups[group].map((tz) => (
-              <option key={tz.label} value={tz.label}>
-                {getCity(tz.label)} {displayCounty(tz.label)} {formatOffset(tz.offset)}
-              </option>
-            ))}
-          </optgroup>
-        ))}
-      </select>
-      <button onClick={() => addTimezone()}>Add</button>
+      <Select
+        ref={selectInputRef}
+        options={groupedOptions}
+        onChange={handleChange}
+        className="add-timezone-select-container"
+        classNamePrefix="add-timezone-select"
+      />
+      <button disabled={!selectValue} className="add-timezone-button" onClick={addTimezone}>
+        Add
+      </button>
     </div>
   )
 }
